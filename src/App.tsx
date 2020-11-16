85d8cf1086b7624bb71052fccca469577aed8b65
Vincent Boisard <boisard.v@gmail.com>
2020-11-16 19:04:58 +0100

    Add support for 6:8 time signature

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import Beats, { Subdivisions } from './Beats';
-import './App.css';
+import React from "react";
+import Beats, { Subdivisions } from "./Beats";
+import "./App.css";
 
 function App() {
-  const [beatCount] = React.useState(4);
+  const [beatCount, setBeats] = React.useState(4);
+  const switchSignature = (newSignature: number) => {
+    return () => {
+      setBeats(newSignature);
+    };
+  };
   const [subdivision, setSubdivision] = React.useState(Subdivisions.Eighth);
   const switchSubdivision = (newSub: Subdivisions) => {
     return () => {
       setSubdivision(newSub);
-    }
-  }
+    };
+  };
 
   return (
     <div className="App">
-      <button onClick={switchSubdivision(Subdivisions.Quarter)}>Quarter notes</button>
-      <button onClick={switchSubdivision(Subdivisions.Eighth)}>Eighth notes</button>
-      <button onClick={switchSubdivision(Subdivisions.Sixteenth)}>Sixteenth notes</button>
+      <button onClick={switchSignature(4)}>4:4</button>
+      <button onClick={switchSignature(6)}>6:8</button>
+      <br />
+      <button onClick={switchSubdivision(Subdivisions.Quarter)}>
+        Quarter notes
+      </button>
+      <button onClick={switchSubdivision(Subdivisions.Eighth)}>
+        Eighth notes
+      </button>
+      <button onClick={switchSubdivision(Subdivisions.Sixteenth)}>
+        Sixteenth notes
+      </button>
 
       <Beats beatCount={beatCount} subdivision={subdivision} />
     </div>
