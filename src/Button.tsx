5e3c52a87223cd781d0464f0734bc1915fae26b0
Vincent Boisard <boisard.v@gmail.com>
2020-12-02 13:07:35 +0100

    Fix tailwind configuration

--- src/Button.tsx
+++ src/Button.tsx
@@ -6,6 +6,13 @@ interface IProps {
   color?: string
 }
 
+// Magic comment to prevent colors from being purged during tree shaking
+// [
+//   "bg-green-500",
+//   "bg-blue-500",
+//   "bg-green-700",
+//   "bg-blue-700",
+// ]
 const Button: FunctionComponent<React.PropsWithChildren<IProps>> = (props) => {
   return (
     <button className={`m-1 py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-${props.color}-500 hover:bg-${props.color}-700`} onClick={props.onClick}>
